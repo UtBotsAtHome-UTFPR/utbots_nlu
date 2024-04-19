3edb33f3a7be9f7033492b387a100708d8a7ed55
gustavo-fardo <gustavo.fardo@hotmail.com>
2024-04-19 11:46:48 -0300

    first attempts at actions and slots

--- actions/actions.py
+++ actions/actions.py
@@ -13,15 +13,32 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 
-class ActionHelloWorld(Action):
+# class ActionHelloWorld(Action):
 
+#     def name(self) -> Text:
+#         return "action_hello_world"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         dispatcher.utter_message(text="Hello World!")
+
+#         return []
+
+class GoToLocation(Action):
     def name(self) -> Text:
-        return "action_hello_world"
-
+        return "action_go_to_location"
+    
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        location_entity = next(tracker.get_latest_entity_values('room'), None)
 
-        dispatcher.utter_message(text="Hello World!")
+        if location_entity:
+            dispatcher.utter_message(text=f"I will navigate to {location_entity}")
+        else:
+            dispatcher.utter_message(text=f"I could not identify the destiny location")
 
         return []
