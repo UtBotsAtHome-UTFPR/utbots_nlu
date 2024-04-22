6f1a3002f7610c8fb07dfb915108345ea9d25197
UtBots-Home <utbots.home@gmail.com>
2024-04-22 15:30:34 -0300

    first attempt at ros actions

--- actions/actions.py
+++ actions/actions.py
@@ -4,27 +4,22 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+class DescribeAmbient(Action):
+    def name(self) -> Text:
+        return "action_describe_ambient"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-# class ActionHelloWorld(Action):
+        dispatcher.utter_message("[ROSACT]DESCRIBE_AMBIENT")
 
-#     def name(self) -> Text:
-#         return "action_hello_world"
-
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-#         dispatcher.utter_message(text="Hello World!")
-
-#         return []
+        return []
 
 class GoToLocation(Action):
     def name(self) -> Text:
--- src/rasa_interface_node.py
+++ src/rasa_interface_node.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import String
+from vision_msgs.msg import BoundingBoxes
 import requests
 import subprocess
 
@@ -12,10 +13,19 @@ class RasaInterface:
 
         # Node initialization
         rospy.init_node('rasa_inteface_node', anonymous=True)
-
+       
         self.sub_usermessage = rospy.Subscriber('/utbots/voice/stt/whispered', String, self.callback_msg)
         self.pub_response = rospy.Publisher('/utbots/voice/tts/robot_speech', String, queue_size=1)
-        
+
+        self.actions_dict = {
+            "[ROSACT]DESCRIBE_AMBIENT": self.describe_ambient
+            }
+
+        # Describe ambient action
+        self.sub_detections = rospy.Subscriber('/utbots/vision/detection/bounding_boxes', BoundingBoxes, self.callback_bounding_box)
+        self.msg_bounding_boxes = BoundingBoxes()
+        self.recieved_msg_bbox = False
+
         self.rate = rospy.Rate(30) # 30hz
 
         self.main()
@@ -28,12 +38,28 @@ class RasaInterface:
             response = requests.post(self.RASA_API_URL, json = payload, headers=headers)
             response = response.json()[0]['text']
             
-            self.msg_response = String()
-            self.msg_response.data = response
-            print(response)
-            self.pub_response.publish(self.msg_response)
+            try:
+                self.actions_dict[response]()
+            except:
+                self.msg_response = String()
+                self.msg_response.data = response
+                print(response)
+                self.pub_response.publish(self.msg_response)
         except:
             rospy.logwarn("[NLU] Rasa not up yet")
+
+    def callback_bounding_box(self, msg):
+        self.msg_bounding_boxes = msg
+        print("recieved")
+        self.recieved_msg_bbox = True
+
+    def describe_ambient(self):
+        while self.recieved_msg_bbox == False:
+            #rospy.loginfo("Waiting for YOLO")
+            pass
+
+        print(self.msg_bounding_boxes)
+        #self.pub_response.publish(self.msg_response)
 
     def main(self):
         while not rospy.is_shutdown():
