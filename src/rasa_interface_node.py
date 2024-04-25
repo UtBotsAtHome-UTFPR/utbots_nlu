79758fb900cd6d07795e57e235e243361c9ad8a4
UtBots-Home <utbots.home@gmail.com>
2024-04-24 21:24:42 -0300

    describe ambient gambiarra working

--- src/rasa_interface_node.py
+++ src/rasa_interface_node.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import String
 from vision_msgs.msg import BoundingBoxes
+from std_srvs.srv import Empty
 from rospkg import RosPack
 import requests
 import subprocess
@@ -19,6 +20,13 @@ class RasaInterface:
         self.sub_usermessage = rospy.Subscriber('/utbots/voice/stt/whispered', String, self.callback_msg)
         self.pub_response = rospy.Publisher('/utbots/voice/tts/robot_speech', String, queue_size=1)
 
+        self.response = ""
+
+        # Describe ambient action
+        self.sub_detections = rospy.Subscriber('/utbots/vision/detection/bounding_boxes', BoundingBoxes, self.callback_bounding_box)
+        self.msg_bounding_boxes = BoundingBoxes()
+        self.recieved_msg_bbox = False
+
         self.rate = rospy.Rate(30) # 30hz
 
         self.main()
@@ -26,23 +34,65 @@ class RasaInterface:
     def callback_msg(self, msg):
         payload = {'message': msg.data}
         headers = {'content-type': 'application/json'}
+        print("request")
 
         try:
-            response = requests.post(self.RASA_API_URL, json = payload, headers=headers)
-            response = response.json()[0]['text']
+            self.response = requests.post(self.RASA_API_URL, json = payload, headers=headers)
+            self.response = self.response.json()[0]['text']
             
-            try:
-                self.actions_dict[response]()
-            except:
+            if self.response != "[ROSACT]DESCRIBE_AMBIENT":
                 self.msg_response = String()
-                self.msg_response.data = response
-                print(response)
+                self.msg_response.data = self.response
+                print(self.response)
                 self.pub_response.publish(self.msg_response)
         except:
             rospy.logwarn("[NLU] Rasa not up yet")
 
+    def callback_bounding_box(self, msg):
+        self.msg_bounding_boxes = msg
+        self.recieved_msg_bbox = True
+        print("recieved")
+
+    def describe_ambient(self):
+        self.recieved_msg_bbox = False
+        self.response = ""
+
+        objects_count = {}
+        msg_yolo_response = String()
+        msg_yolo_response.data = "I see the following: "
+        for bbox in self.msg_bounding_boxes.bounding_boxes:
+            if bbox.Class in objects_count:
+                objects_count[bbox.Class] += 1
+            else:
+                objects_count[bbox.Class] = 1
+
+        for object, count in objects_count.items():
+            msg_yolo_response.data += ", " + str(count) + " " + object
+
+        print(msg_yolo_response)
+
+        self.pub_response.publish(msg_yolo_response)
+
     def main(self):
         while not rospy.is_shutdown():
+            if self.response == "[ROSACT]DESCRIBE_AMBIENT":
+                print(self.recieved_msg_bbox)
+                if self.recieved_msg_bbox == True:
+                    self.describe_ambient()
+                else:
+                    rospy.wait_for_service('/yolov8_server')
+                    try:
+                        # Create a proxy to call the service
+                        empty_service_proxy = rospy.ServiceProxy('/yolov8_server', Empty)
+
+                        # Call the service
+                        response = empty_service_proxy()
+
+                        # Print response if needed
+                        print("Service call succeeded")
+                    except rospy.ServiceException as e:
+                        print("Service call failed:", e)
+
             self.rate.sleep()
 
 if __name__ == "__main__":
