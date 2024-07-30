a57084b90887d51994a92d1e96432efb76bf54ed
David Segalle <davidsegalle@alunos.utfpr.edu.br>
2024-07-30 13:59:59 -0300

    nlu action added, missing changes in cmakelists

--- src/rasa_interface_node.py
+++ src/rasa_interface_node.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 import rospy
+import roslib
+roslib.load_manifest('utbots_nlu')
 from std_msgs.msg import String
 from vision_msgs.msg import BoundingBoxes
 from std_srvs.srv import Empty
 from rospkg import RosPack
+import actionlib
+from utbots_actions.msg import InterpretNLUAction, InterpretNLUResult
 import requests
 import subprocess
 import json
@@ -18,18 +22,18 @@ class RasaInterface:
 
         # Node initialization
         rospy.init_node('rasa_inteface_node', anonymous=True)
-       
+        # Action server initialization
+        self.server = actionlib.SimpleActionServer('interpret_nlu', InterpretNLUAction, self.execute, False)
+        self.server.start()
+
+        # Publishers and Subscribers
         self.sub_usermessage = rospy.Subscriber('/utbots/voice/stt/whispered', String, self.callback_msg)
         self.pub_response = rospy.Publisher('/utbots/voice/tts/robot_speech', String, queue_size=1)
         self.pub_action = rospy.Publisher('/utbots/voice/nlu/action', String, queue_size=1)
         self.pub_slot = rospy.Publisher('/utbots/voice/nlu/slot', String, queue_size=1)
 
         self.response = ""
-
-        # Describe ambient action
-        self.sub_detections = rospy.Subscriber('/utbots/vision/detection/bounding_boxes', BoundingBoxes, self.callback_bounding_box)
-        self.msg_bounding_boxes = BoundingBoxes()
-        self.recieved_msg_bbox = False
+        self.enable_nlu = False
 
         self.rate = rospy.Rate(30) # 30hz
 
@@ -44,33 +48,38 @@ class RasaInterface:
             rospy.loginfo(f"[NLU] Response: {self.response}")
             try:
                 extracted_data = json.loads(self.response)
+                action_res = InterpretNLUResult()
                 if 'ROSACT' in extracted_data or 'SLOT' in extracted_data:
                     if extracted_data["ROSACT"] != "":
-                        msg_action = String()
-                        msg_action.data = extracted_data["ROSACT"]
-                        self.pub_action.publish(msg_action)
+                        action_res.Task = extracted_data["ROSACT"]
                     if extracted_data["SLOT"] != "":
-                        msg_slot = String()
-                        msg_slot.data = extracted_data["SLOT"]
-                        self.pub_slot.publish(msg_slot)
+                        action_res.Data = extracted_data["SLOT"]
+                else:
+                    action_res.Task = ""
+                    action_res.Data = ""
             except:
                 self.msg_response = String()
                 self.msg_response.data = self.response
                 self.pub_response.publish(self.msg_response)
         except IndexError:
             rospy.logwarn("[NLU] No response for this request")
+            self.server.set_aborted()
         except KeyError as e:
             rospy.logwarn(f"[NLU] Error: {e}")
+            self.server.set_aborted()
         except Exception as e:
             if str(type(e)) == "<class 'requests.exceptions.ConnectionError'>":
                 rospy.logwarn("[NLU] Rasa not up yet")
+                self.server.set_aborted()
             else:
                 rospy.logwarn(f"[NLU] An unexpected error occurred: {e}")
+                self.server.set_aborted()
 
-    def callback_bounding_box(self, msg):
-        self.msg_bounding_boxes = msg
-        self.recieved_msg_bbox = True
-        print("recieved")
+        self.enable_nlu = False
+        self.server.set_succeeded(action_res)
+
+    def execute(self, goal):
+        self.enable_nlu = True
 
     def main(self):
         while not rospy.is_shutdown():
